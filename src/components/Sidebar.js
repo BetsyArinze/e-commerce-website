3f1c2bca7e4d82bc653c675db44554de2488a448
Betsy Arinze <betarinze@gmail.com>
2023-10-06 12:38:02 +0100

    Product component added

---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React, {useState, createContext} from 'react';
+
+//create context
+export const SidebarContext = createContext()
+
+const SidebarProvider = {
+
+};
+
+export default SidebarProvider;
